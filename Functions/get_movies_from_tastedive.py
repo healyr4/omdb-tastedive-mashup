8736f56857726cfe97ca29e049cf483c2db7c50e
healyr4 <healyr4@tcd.ie>
2020-09-09 13:54:52 +0100

    Update get_movies_from_tastedive.py

--- Functions/get_movies_from_tastedive.py
+++ Functions/get_movies_from_tastedive.py
@@ -1,15 +1,21 @@
-#
+
 import requests_with_caching
+import json
 #q= search query
 #limit = max no. of reccomendations
 def get_movies_from_tastedive (movie):
     baseurl = "https://tastedive.com/api/similar"
     param_dict = {}
-    param_dict['q'] = movie
-    param_dict['type'] = "movies"
-    param_dict['limit'] = 5
+    param_dict["q"] = movie
+    param_dict["type"] = "movies"
+    param_dict["limit"] = 5
     
-    tastedive_resp = requests_with_caching.get(baseurl, params = param_dict, )
+    tastedive_resp = requests_with_caching.get(baseurl, params =param_dict)
+    print(tastedive_resp.url)
+    return tastedive_resp.json()
+    
 # some invocations that we use in the automated tests; uncomment these if you are getting errors and want better error messages
-# get_movies_from_tastedive("Bridesmaids")
-# get_movies_from_tastedive("Black Panther")
+#et_movies_from_tastedive("Bridesmaids")
+#et_movies_from_tastedive("Black Panther")
+
+
